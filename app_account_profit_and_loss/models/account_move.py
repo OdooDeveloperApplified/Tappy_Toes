165cc50eec0867a013f9c016b94023e9d62b3ed1
OdooDeveloperApplified <odooapplified@gmail.com>
2026-08-22 13:20:08 +0530

    Restrict some fields and views to specific admin manager

--- app_account_profit_and_loss/models/account_move.py
+++ app_account_profit_and_loss/models/account_move.py
@@ -6,3 +6,10 @@ class AccountReport(models.Model):
     _inherit = 'account.move'
 
     file_upload = fields.Image(string="File Upload", max_width=1600, max_height=1600)
+    is_restricted_manager = fields.Boolean(string="Is Restricted Manager", compute="_compute_is_restricted_manager")
+
+    @api.depends('company_id')
+    def _compute_is_restricted_manager(self):
+        for move in self:
+            move.is_restricted_manager = self.env.user.has_group('app_account_profit_and_loss.group_hide_delete_account_move')
+            _logger.info("Evaluating Restricted Manager for Bill %s: %s", move.id, move.is_restricted_manager)
